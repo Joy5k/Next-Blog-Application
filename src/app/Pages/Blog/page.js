2f71d79ab68b510ad8589b0ca0c02e2432e7f888
Joy <mmehedihasanjoyv@gmail.com>
2023-08-21 00:06:22 +0600

    setup basic stucture

---
+++ src/app/Pages/Blog/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+    return (
+        <div className="h-screen">
+            <h3 className="text-5xl text-center text-gray-700 font-bold">The Blog  page</h3>
+        </div>
+    );
+};
+
+export default page;
---
+++ src/app/page.js
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <main className="">
+      <h3 className="text-5xl font-bold underline text-green-400">This is main page</h3>
+    </main>
+  )
+}
